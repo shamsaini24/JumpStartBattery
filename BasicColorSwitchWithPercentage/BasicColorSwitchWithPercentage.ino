e92961e5bad18b396666882e290b092cf3195174
Sham Saini <32149321+shamsaini24@users.noreply.github.com>
2024-07-12 21:10:35 -0700

    Updated code to display the value more reliably without artifacts

--- BasicColorSwitchWithPercentage/BasicColorSwitchWithPercentage.ino
+++ BasicColorSwitchWithPercentage/BasicColorSwitchWithPercentage.ino
@@ -6,7 +6,7 @@ CST816S touch(6, 7, 13, 5); // sda, scl, rst, irq
 
 int currentColor = 0;       // 0 = green, 1 = yellow, 2 = red
 bool isTouched = false;     // To keep track of touch status
-int percentage = 80;
+int percentage = 7;
 
 void setup() {
   Serial.begin(115200);
@@ -14,15 +14,15 @@ void setup() {
 
   tft.init();                // Initialize the display
   tft.setRotation(0);        // Set display orientation, adjust if necessary
-  tft.fillScreen(TFT_GREEN); // Start with green screen
+  //tft.fillScreen(TFT_GREEN); // Start with green screen
 
   // Set text color and background color
-  tft.setTextColor(TFT_WHITE, TFT_GREEN);
+  //tft.setTextColor(TFT_WHITE, TFT_GREEN);
 
   // Set text font and size
   tft.setFreeFont(&FreeSansBold18pt7b);
 
-  drawText();
+  changeColor();
 }
 
 void loop() {
@@ -44,8 +44,8 @@ void loop() {
           percentage++;
       }
 
-    Serial.print("X = "); Serial.print(x);
-    Serial.print("\tY = "); Serial.println(y);
+    //Serial.print("X = "); Serial.print(x);
+    //Serial.print("\tY = "); Serial.println(y);
     changeColor();
     delay(200); // Add delay to avoid rapid color changes
 
@@ -58,29 +58,20 @@ void changeColor() {
   //currentColor = (currentColor + 1) % 3; // Cycle through 0, 1, 2
 
   if (percentage > 60) {
-     tft.fillScreen(TFT_GREEN);
-     tft.setTextColor(TFT_WHITE, TFT_GREEN);
+    tft.fillScreen(TFT_GREEN);
+    tft.setTextColor(TFT_WHITE, TFT_GREEN);
   } else if (percentage > 30 && percentage <= 60){
     tft.fillScreen(TFT_YELLOW);
     tft.setTextColor(TFT_BLACK, TFT_YELLOW);
-  } else {
-     tft.fillScreen(TFT_RED);
-     tft.setTextColor(TFT_WHITE, TFT_RED);
+  } else if(percentage > 10 && percentage <= 30){
+    tft.fillScreen(TFT_RED);
+    tft.setTextColor(TFT_WHITE, TFT_RED);
+  } else{
+    tft.fillScreen(TFT_RED);
+    tft.setTextColor(TFT_WHITE, TFT_RED);
+    Serial.println("SHOULD FLASH HERE \r\n");
+
   }
-  // switch (currentColor) {
-  //   case 0:
-  //     tft.fillScreen(TFT_GREEN);
-  //     tft.setTextColor(TFT_WHITE, TFT_GREEN);
-  //     break;
-  //   case 1:
-  //     tft.fillScreen(TFT_YELLOW);
-  //     tft.setTextColor(TFT_BLACK, TFT_YELLOW);
-  //     break;
-  //   case 2:
-  //     tft.fillScreen(TFT_RED);
-  //     tft.setTextColor(TFT_WHITE, TFT_RED);
-  //     break;
-  // }
 
   drawText();
 }
@@ -91,7 +82,8 @@ void drawText() {
 
   // Set the text datum to the center
   tft.setTextDatum(MC_DATUM);
-
+  tft.setTextPadding(tft.width());
   // Draw the text in the center of the screen
   tft.drawString(text, tft.width() / 2, tft.height() / 2);
+  
 }
